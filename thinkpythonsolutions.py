c4388c6ccc6cd3bb336339c8e52fa7b692c210b1
T Cavanagh <cavanaghtd@gmail.com>
2019-08-06 07:18:47 +1000

    Exercise 6.4

--- thinkpythonsolutions.py
+++ thinkpythonsolutions.py
@@ -134,3 +134,49 @@ prompt()
 
 # Exercise 5.6
 
+
+
+# Exercise 6.4
+
+def b(z):
+    prod = a(z, z)
+    print(z, prod)
+    return prod
+
+def a(x, y):
+    x=x+1
+    return x * y
+
+def c(x, y, z):
+    total = x + y + z
+    square = b(total)**2
+    return square
+
+x=1
+y=x+1
+print(c(x, y+3, x+y))
+
+# Draw a stack diagram for the program. What does the program print?
+'''
+main:
+    x ---> 1
+    y ---> 2
+c:
+    x ---> 1
+    y ---> 5
+    z ---> 3
+    total ---> 9
+    square ---> 8100
+b:
+    z ---> 9
+    prod ---> 90
+a:
+    x ---> 9
+    y ---> 9
+    x ---> 10
+ 
+Program prints:
+9 90
+8100   
+'''
+
