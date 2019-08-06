b37c26c24b319337e08d4ed9e4f7260a667bf6ae
T Cavanagh <cavanaghtd@gmail.com>
2019-08-07 06:57:52 +1000

    Exercise 6.5

--- thinkpythonsolutions.py
+++ thinkpythonsolutions.py
@@ -180,3 +180,15 @@ Program prints:
 8100   
 '''
 
+#Exercise 6.5 Ackerman function
+
+def ack(m, n):
+    if m == 0:
+        return n + 1
+    elif m > 0 and n == 0:
+        return ack(m - 1, 1)
+    elif m > 0 and n > 0:
+        return ack(m - 1, ack(m, n - 1))
+
+print(ack(3, 4))
+
